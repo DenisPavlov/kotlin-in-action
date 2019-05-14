17e8e616e8e40036946cce2e138161fe2da3550a
denis <denispavlov1989@gmail.com>
2019-05-14 23:45:50 +0700

    chapter 4

--- src/ch04/4.1.5_1_SealedClasses.kt
+++ src/ch04/4.1.5_1_SealedClasses.kt
@@ -8,8 +8,7 @@ fun eval(e: Expr): Int =
     when (e) {
         is Num -> e.value
         is Sum -> eval(e.right) + eval(e.left)
-        else ->
-            throw IllegalArgumentException("Unknown expression")
+        else -> throw IllegalArgumentException("Unknown expression")
     }
 
 fun main(args: Array<String>) {
