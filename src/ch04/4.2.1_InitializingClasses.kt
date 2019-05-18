8d4db0c53ebf6e6a65e356809b1281d5c9cafa24
denis <denispavlov1989@gmail.com>
2019-05-18 20:42:13 +0700

    chapter 4

--- src/ch04/4.2.1_InitializingClasses.kt
+++ src/ch04/4.2.1_InitializingClasses.kt
@@ -1,13 +1,15 @@
 package ch04.ex2_1_InitializingClasses
 
-class User(val nickname: String,
-           val isSubscribed: Boolean = true)
+class User(
+    val nickname: String,
+    val isSubscribed: Boolean = true
+)
 
 fun main(args: Array<String>) {
     val alice = User("Alice")
     println(alice.isSubscribed)
     val bob = User("Bob", false)
     println(bob.isSubscribed)
-    val carol = User("Carol", isSubscribed = false)
+    val carol = User(nickname = "Carol", isSubscribed = false)
     println(carol.isSubscribed)
 }
