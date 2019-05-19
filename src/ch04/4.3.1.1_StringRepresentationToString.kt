642867402a57939b03e72c1998fe4060f4e52383
denis <denispavlov1989@gmail.com>
2019-05-19 19:12:39 +0700

    change Kotlin version to 1.3

--- src/ch04/4.3.1.1_StringRepresentationToString.kt
+++ src/ch04/4.3.1.1_StringRepresentationToString.kt
@@ -1,7 +1,7 @@
 package ch04.ex3_1_1_StringRepresentationToString
 
 class Client(val name: String, val postalCode: Int) {
-    override fun toString() = "Client(name=$name, postalCode=$postalCode)"
+//    override fun toString() = "Client(name=$name, postalCode=$postalCode)"
 }
 
 fun main(args: Array<String>) {
