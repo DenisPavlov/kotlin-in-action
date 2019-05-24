9d064460559d34f3a188cebc90b91f2971b61352
denis <denispavlov1989@gmail.com>
2019-05-24 23:21:48 +0700

    end chapter 5

--- src/ch05/5.3.2_2_CreatingSequences1.kt
+++ src/ch05/5.3.2_2_CreatingSequences1.kt
@@ -3,9 +3,9 @@ package ch05.ex3_2_2_CreatingSequences1
 import java.io.File
 
 fun File.isInsideHiddenDirectory() =
-        generateSequence(this) { it.parentFile }.any { it.isHidden }
+    generateSequence(this) { it.parentFile }.any { it.isHidden }
 
-fun main(args: Array<String>) {
+fun main() {
     val file = File("/Users/svtk/.HiddenDir/a.txt")
     println(file.isInsideHiddenDirectory())
 }
--- src/ch05/5.5.1_1_Alphabet.kt
+++ src/ch05/5.5.1_1_Alphabet.kt
@@ -3,12 +3,12 @@ package ch05.Alphabet
 fun alphabet(): String {
     val result = StringBuilder()
     for (letter in 'A'..'Z') {
-         result.append(letter)
+        result.append(letter)
     }
     result.append("\nNow I know the alphabet!")
     return result.toString()
 }
 
-fun main(args: Array<String>) {
+fun main() {
     println(alphabet())
 }
