9d94b49a4f4ba1355bf4e6a6ec424f4a6672ca53
denis <denispavlov1989@gmail.com>
2019-05-24 23:54:50 +0700

    end chapter 6.1.5

--- src/ch06/6.1.4_2_ElvisOperator1.kt
+++ src/ch06/6.1.4_2_ElvisOperator1.kt
@@ -1,7 +1,9 @@
 package ch06.ex1_4_2_ElvisOperator1
 
-class Address(val streetAddress: String, val zipCode: Int,
-              val city: String, val country: String)
+class Address(
+    val streetAddress: String, val zipCode: Int,
+    val city: String, val country: String
+)
 
 class Company(val name: String, val address: Address?)
 
@@ -9,14 +11,14 @@ class Person(val name: String, val company: Company?)
 
 fun printShippingLabel(person: Person) {
     val address = person.company?.address
-      ?: throw IllegalArgumentException("No address")
-    with (address) {
+        ?: throw IllegalArgumentException("No address")
+    with(address) {
         println(streetAddress)
         println("$zipCode $city, $country")
     }
 }
 
-fun main(args: Array<String>) {
+fun main() {
     val address = Address("Elsestr. 47", 80687, "Munich", "Germany")
     val jetbrains = Company("JetBrains", address)
     val person = Person("Dmitry", jetbrains)
