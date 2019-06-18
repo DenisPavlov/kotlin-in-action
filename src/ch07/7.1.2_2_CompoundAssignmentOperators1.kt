a1c72d660bf49345a973049eeaf90fc82596260b
denis <denispavlov1989@gmail.com>
2019-06-18 23:41:05 +0700

    start read chapter 7

--- src/ch07/7.1.2_2_CompoundAssignmentOperators1.kt
+++ src/ch07/7.1.2_2_CompoundAssignmentOperators1.kt
@@ -2,7 +2,7 @@ package ch07.ex1_2_2_CompoundAssignmentOperators1
 
 import java.util.ArrayList
 
-fun main(args: Array<String>) {
+fun main() {
     val numbers = ArrayList<Int>()
     numbers += 42
     println(numbers[0])
